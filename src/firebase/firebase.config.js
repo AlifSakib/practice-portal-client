84617301a51b3bd93a2fecafcb7542923053513a
AlifSakib <alifsakib@gmail.com>
2022-11-16 13:55:40 +0600

    add all practice files

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB4PGoLN-djpQf8P7h1Gf76Znxsczi1h9k",
+  authDomain: "ema-john-practice-4e85d.firebaseapp.com",
+  projectId: "ema-john-practice-4e85d",
+  storageBucket: "ema-john-practice-4e85d.appspot.com",
+  messagingSenderId: "726242737317",
+  appId: "1:726242737317:web:dab8ec5049497cffe2e14c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
