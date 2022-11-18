24a3214c1a35de84e7bba7f931423d8d6eae6ffb
AlifSakib <alifsakib@gmail.com>
2022-11-19 00:20:28 +0600

    added select option on add doctor page

--- src/Pages/Dashboard/AddDoctor/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor/AddDoctor.js
@@ -1,7 +1,74 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const AddDoctor = () => {
-  return <div>AddDoctor</div>;
+  const handleAddDoctor = (data) => {
+    console.log(data);
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  return (
+    <div>
+      <div className="h-[800px] flex justify-center items-center">
+        <div className="w-96 p-7">
+          <h2 className="text-xl text-center">Add A Doctor</h2>
+          <form onSubmit={handleSubmit(handleAddDoctor)}>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                {" "}
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                {...register("name", {
+                  required: "Name is Required",
+                })}
+                className="input input-bordered w-full max-w-xs"
+              />
+              {errors.name && (
+                <p className="text-red-500">{errors.name.message}</p>
+              )}
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                {" "}
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                {...register("email", {
+                  required: true,
+                })}
+                className="input input-bordered w-full max-w-xs"
+              />
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                {" "}
+                <span className="label-text">Secialty</span>
+              </label>
+              <select className="select select-bordered w-full max-w-xs">
+                <option disabled selected>
+                  Select a spcialty
+                </option>
+                <option>Han Solo</option>
+                <option>Greedo</option>
+              </select>
+            </div>
+
+            <input
+              className="btn btn-accent w-full mt-4"
+              defaultValue="Add Doctor"
+              type="submit"
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddDoctor;
